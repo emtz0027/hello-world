534b147bef4a36dcaec0fa321d3f5d67e949f3e3
Estephania <52553333+emtz0027@users.noreply.github.com>
2021-02-17 23:29:16 -0600

    Add files via upload
    Program will print out "Hello World" 100 times

---
+++ problem1.py
@@ -0,0 +1,15 @@
+#Estephania Martinez 02/17/21
+#Problem 1
+
+
+#Will print out "Hello World" 100 times
+
+#for loop starts here while giving a range of
+#how many times we want it to print
+for x in range(100):
+
+    #identifying what we want the program to print out
+    x = "Hello World"
+
+    #program will proceed to print "Hello World" 100 times
+    print(x)
