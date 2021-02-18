3abd3b60a7583699474ee3f771d9757c00310cf1
Estephania <52553333+emtz0027@users.noreply.github.com>
2021-02-17 23:30:36 -0600

    Add files via upload
    Program will print out the even number between 1 and 25.

---
+++ problem2.py
@@ -0,0 +1,13 @@
+#Estephania Martinez 02/17/21
+#Probelm 2
+
+#Will print out the even number between 1 and 25
+
+#for loop starts here
+#listed the number between 1 and 25
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+          13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25]:
+    #any number divisible by 2 will print out
+    if x % 2 == 0:
+        print(x)
