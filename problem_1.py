87ef6e9c7450816679b7e4b0044c7210436543bc
Estephania <52553333+emtz0027@users.noreply.github.com>
2021-03-11 11:17:42 -0600

    Add files via upload

---
+++ problem_1.py
@@ -0,0 +1,5 @@
+#Estephania Martinez 3/11/21 Module 9 Quiz 
+
+#ask for name and print out a greeting
+name = input("What is your name?")
+print("Hello,", name, "how are you?")
---
+++ problem_2.py
@@ -0,0 +1,8 @@
+#Estephania Martinez 3/11/21 Problem 2
+
+#Take a number and rint out Hello World said number of times
+
+num = int(input("Type in a number"))
+
+for i in range(num):
+    print("Hello World")
