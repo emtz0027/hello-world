0d070d74a2ce01295a70c372525685ca95b18e5b
Estephania <52553333+emtz0027@users.noreply.github.com>
2021-03-11 11:34:39 -0600

    Add files via upload

---
+++ problem_3.py
@@ -0,0 +1,22 @@
+
+#Estephania Martinez 3/11/21 Problem 3
+
+import random
+#generate a random number
+#if divisible by 3, print "Fizz"
+#if divisible by 5, print "Buzz
+#other, print the number
+
+
+
+def FizzBuzz(x):
+    if x%3 == 0:
+        print("Fizz")
+    elif x%5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+
+FizzBuzz()
+
+#not finished
